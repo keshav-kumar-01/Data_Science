244b6257c16da640fc961988233db98d4555aef6
keshav Kumar <keshavkumarhf@gmail.com>
2026-02-11 12:29:46 +0530

    test.py

---
+++ test.py
@@ -0,0 +1,8 @@
+def maxSubarraySum(arr,k):
+    window_sum = sum(arr[:k])
+    max_sum = window_sum
+    for i in range(k,len(arr)):
+        window_sum = window_sum + arr[i]
+        window_sum = window_sum - arr[i-k]
+        max_sum = max(max_sum,window_sum)
+    return max_sum
